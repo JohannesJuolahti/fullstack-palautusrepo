02b7badcdc689a4efcf2e4e86c2b92c7833baf99
Johannes J <johannes.v.juolahti@utu.fi>
2021-04-10 11:27:06 +0300

    Osa 1. 1.8.

--- osa1/unicafe/1.11.js
+++ osa1/unicafe/1.11.js
@@ -11,6 +11,20 @@ const getWeightedValue = (weight, value) => {
   return weight * value
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <Display text={props.statistics.good_text} value={props.good} />
+      <Display text={props.statistics.neutral_text} value={props.neutral} />
+      <Display text={props.statistics.bad_text} value={props.bad} />
+      <Display text={props.statistics.all_text} value={props.all} />
+      <Display text={props.statistics.average_text} value={props.statistics.average} />
+      <Display text={props.statistics.positive_text} value={props.statistics.positive} percent={props.statistics.percent} />
+    </div>
+    )
+  }
+  
+
 const Header = (props) => {
   return (
     <div>
@@ -25,37 +39,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all_points = good + bad + neutral
-  const average = ((getWeightedValue(1, good) 
-  + getWeightedValue(0, neutral) 
-  + getWeightedValue(-1, bad))) / all_points || 0
-  const positive = (getWeightedValue(1, good) / all_points) * 100 || 0
-
-
   const header_text = "give feedback"
-  const stats = "statistics"
-  const good_text = "good"
-  const neutral_text = "neutral"
-  const bad_text = "bad"  
-  const all_text = "all"
-  const average_text = "average"
-  const positive_text = "positive"
-  const percent = "%"
+  const all_points = good + bad + neutral
+  const weight_good = 1
+  const weight_neutral = 0
+  const weight_bad = -1
 
+  const statistics = {
+  average: ((getWeightedValue(weight_good, good)
+  + getWeightedValue(weight_neutral, neutral)
+  + getWeightedValue(weight_bad, bad))) / all_points || 0,
+  positive: (getWeightedValue(1, good) / all_points) * 100 || 0,
+  stats: "statistics",
+  good_text: "good",
+  neutral_text: "neutral",
+  bad_text: "bad",
+  all_text: "all",
+  average_text: "average",
+  positive_text: "positive",
+  percent: "%",
+  }
 
   return (
     <div>
       <Header header={header_text} />
-      <Button handleClick={() => setGood(good + 1)} text={good_text} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={neutral_text} />
-      <Button handleClick={() => setBad(bad + 1)} text={bad_text} />
-      <Header header={stats} />
-      <Display text={good_text} value={good} />
-      <Display text={neutral_text} value={neutral} />
-      <Display text={bad_text} value={bad} />
-      <Display text={all_text} value={all_points} />
-      <Display text={average_text} value={average} />
-      <Display text={positive_text} value={positive} percent={percent} />
+      <Button handleClick={() => setGood(good + 1)} text={statistics.good_text} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={statistics.neutral_text} />
+      <Button handleClick={() => setBad(bad + 1)} text={statistics.bad_text} />
+      <Header header={statistics.stats} />
+      <Statistics statistics={statistics} good={good} bad={bad} neutral={neutral} all={all_points}/>
     </div>
   )
 }

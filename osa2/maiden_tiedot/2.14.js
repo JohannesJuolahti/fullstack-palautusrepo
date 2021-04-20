49de1d34b32a20bed810601b5573bef23fcb42e8
Johannes J <johannes.v.juolahti@utu.fi>
2021-04-20 14:46:26 +0300

    Osa 2. 2.14.

---
+++ osa2/maiden_tiedot/2.14.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = (props) => {
+  return (
+    <div>
+        Find countries with: <input value={props.newFilter} onChange={props.handleFilterChange}/>
+    </div>
+  )
+}
+
+const Weather = ({capital}) => {
+  const [weather, setWeather] = useState({location:{}, current: {}});
+  const [loading, setLoading] = useState(true);
+  const key = process.env.REACT_APP_WEATHERSTACK_CODE
+  const url = `http://api.weatherstack.com/current?access_key=${key}&query=${capital}`
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setLoading(false);
+      setWeather(response.data)
+    })
+  }, [])
+
+  return loading ? <p>Loading...</p> : (
+    <div>
+      <h2>Weather in {weather.location.name}</h2>
+      <p><b>Temperature:</b> {weather.current.temperature} Celcius</p>
+      <img src = {weather.current.weather_icons} />
+      <p><b>Wind:</b> {weather.current.wind_speed} kph direction {weather.current.wind_dir}</p>
+    </div>
+  )
+}
+
+const Country = (props) => {
+  return (
+    <div>
+      {props.name}
+    </div>
+  )
+}
+
+const CountryInfo = (props) => {
+  return (
+    <div>
+      <h1>{props.nameAsHeader}</h1>
+      <p>Capital: {props.capital}</p>
+      <p>Capital: {props.population}</p>
+      <h2>Spoken languages</h2>
+      {props.languages.map(language => 
+        <ul key={language['name']}>
+          <li>{language['name']} </li>
+        </ul>
+      )}
+      <img src={props.flag} width="150" height="150" alt='Country flag.'/>
+    </div>
+  )
+}
+
+const Countries = (props) => {
+  if (props.countriesToShow.length > 10) {
+    return (
+      <p>Too many matches or empty filter, specify another filter.</p>
+    )
+  } else if (props.countriesToShow.length > 1) {
+    return (
+      <div>
+          {props.countriesToShow.map(country =>
+            <Country key={country.name} name={country.name}/>
+          )}
+      </div>
+    )
+  } else if (Array.isArray(props.countriesToShow) && props.countriesToShow.length) {
+    let onlyCountry = props.countriesToShow[0]
+    return (
+      <div>
+        
+        <CountryInfo capital={onlyCountry['capital']} population={onlyCountry['population']}
+        languages={onlyCountry['languages']} flag={onlyCountry['flag']} 
+        nameAsHeader={onlyCountry['name']}/>
+        <Weather capital={onlyCountry['capital']} />
+
+      </div>
+    )
+  }
+  return (<p>Loading countries...</p>)
+}
+
+const App = () => {
+
+  const [ newFilter, setNewFilter ] = useState('')
+  const [ countries, setCountries] = useState([])
+  const [showAll] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const countriesToShow = showAll ? countries : countries.filter(country => 
+    country.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+  
+  return (
+    <div>
+      
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <Countries countriesToShow={countriesToShow}/>
+
+    </div>
+  )
+}
+
+export default App

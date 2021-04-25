83375ad78fb0a538bb11749b6673e44ba00cd73b
Johannes J <johannes.v.juolahti@utu.fi>
2021-04-25 22:49:05 +0300

    Osa 2. 2.18.

--- osa2/puhelinluettelo/services/person.js
+++ osa2/puhelinluettelo/services/person.js
@@ -13,12 +13,19 @@ const create = newObject => {
 
 const del = (id) => {
   axios.delete(baseUrl.concat(`/${id}`))
+
 } 
+
+const update = (id, number) => {
+  axios.put(baseUrl.concat(`/${id}`), number)
+  
+}
 
 const personService = {
     getAll,
     create,
     del,
+    update,
   };
 
   export default personService;

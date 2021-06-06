70ccfe49fe528663dbf469e33361b6a0ba8608ea
Johannes J <johannes.v.juolahti@utu.fi>
2021-06-06 16:04:53 +0300

    Osa 3. 3.4

--- osa3/3.x/index.js
+++ osa3/3.x/index.js
@@ -48,6 +48,13 @@ let persons = [
       }
 
   })
+
+  app.delete('/api/persons/delete/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
